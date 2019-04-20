8a7f08e2ff41fcc98431b5c73b7b6099956556cc
Julian Mendez <julian.mendez@gmail.com>
2019-04-20 18:50:10 +0200

    Improve error message in normalization extension

--- tabulas-ext/src/main/scala/de/tudresden/inf/lat/tabulas/ext/renderer/NormalizationExtension.scala
+++ tabulas-ext/src/main/scala/de/tudresden/inf/lat/tabulas/ext/renderer/NormalizationExtension.scala
@@ -4,12 +4,14 @@ package de.tudresden.inf.lat.tabulas.ext.renderer
 import java.io._
 import java.util.Objects
 
+import de.tudresden.inf.lat.tabulas.datatype.ParseException
 import de.tudresden.inf.lat.tabulas.ext.normalizer.{JsonNormalizer, Normalizer, PropertiesNormalizer, YamlNormalizer}
 import de.tudresden.inf.lat.tabulas.ext.parser.MultiParser
 import de.tudresden.inf.lat.tabulas.extension.Extension
+import de.tudresden.inf.lat.tabulas.parser.ParserConstant
 import de.tudresden.inf.lat.tabulas.table.TableMap
 
-import scala.util.Try
+import scala.util.{Failure, Success, Try}
 
 /** Normalization extension. It reads and writes using the same file.
   *
@@ -19,6 +21,11 @@ case class NormalizationExtension() extends Extension {
   final val Name: String = "normalize"
   final val Help: String = "(input) : normalize a Tabula file in its own format (e.g. Tabulas/JSON -> Tabulas/JSON, Tabula/YAML -> Tabula/YAML)"
   final val RequiredArguments: Int = 1
+
+  final val HyphenChar = "-"
+  final val ColonChar = ":"
+  final val Space = ParserConstant.Space
+  final val NewLine = ParserConstant.NewLine
 
   override def process(arguments: Seq[String]): Try[Boolean] = Try {
     val result = if (Objects.isNull(arguments) || arguments.size != RequiredArguments) {
@@ -35,18 +42,36 @@ case class NormalizationExtension() extends Extension {
         .map(normalizer => NormalizerPair(normalizer, normalizer.parse(new StringReader(content))))
         .seq
 
-      val bestNormalizerPair = res
+      val maybeBestNormalizerPair = res
         .find(pair => pair.content.isSuccess)
-        .getOrElse(res.head)
 
-      val tableMap = bestNormalizerPair.content.get
-      val bestNormalizer = bestNormalizerPair.normalizer
+      val acceptedNormalizerPair = maybeBestNormalizerPair match {
+        case Some(theFirstNormalizerPair) => theFirstNormalizerPair
+        case None =>
+          val newMessage = res.map(pair => pair.content match {
+            case Failure(exception) =>
+              formatMessage(pair.normalizer, exception.getMessage)
+
+            case Success(_) =>
+              throw new IllegalStateException("Illegal state in normalization extension.")
+
+          }).mkString(ParserConstant.NewLine)
+          throw ParseException(newMessage)
+      }
+
+      val tableMap = acceptedNormalizerPair.content.get
+      val normalizer = acceptedNormalizerPair.normalizer
 
       val output = new BufferedWriter(new FileWriter(outputFileName))
-      bestNormalizer.render(output, tableMap)
+      normalizer.render(output, tableMap)
       true
     }
     result
+  }
+
+  def formatMessage(normalizer: Normalizer, message: String): String = {
+    NewLine + HyphenChar + Space + normalizer.getClass.getSimpleName +
+      Space + ColonChar + Space + message
   }
 
   override def getExtensionName: String = {
